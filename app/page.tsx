0963121682bddbef3233f19be5b0232428235dd4
Donizete Queiroz <webmaster@zapnetweb.com.br>
2024-11-12 03:36:10 -0300

    chore: add tailwind prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
-const Home = ()  => {
-  return <h1>Ola Mundo</h1>;
+const Home = () => {
+  return (
+    <div className="flex w-screen items-center justify-center">
+      <h1 className="text-red-500" p-5>
+        Excepcional dia
+      </h1>
+    </div>
+  );
 };
 
 export default Home;
