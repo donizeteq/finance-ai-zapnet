43696bdac8485327f7a2d18a061a97c11b132b4c
Donizete Queiroz <webmaster@zapnetweb.com.br>
2024-11-12 04:16:38 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,7 @@
+import { Button } from "./_components/ui/button";
+
 const Home = () => {
-  return (
-    <div className="flex w-screen items-center justify-center">
-      <h1 className="text-red-500" p-5>
-        Excepcional dia
-      </h1>
-    </div>
-  );
+  return <Button>excepcional dia</Button>;
 };
 
 export default Home;
